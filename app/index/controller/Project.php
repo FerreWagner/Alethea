f70db10fffdb214673d33afb06c1a3a2da4843b9
FerreWagner <18408229270@163.com>
2019-02-04 17:34:54 +0800

    2019 last commit for Alethea

--- app/index/controller/Project.php
+++ app/index/controller/Project.php
@@ -11,7 +11,7 @@ class Project extends Controller
          * header posts
          */
         $recent   = db('article')->field('id,title')->order('order', 'desc')->where(['type' => config('article_type.projects')])->limit(3)->select();
-        $products = db('article')->field('id,title,order,pic,time,keywords,see,desc')->where(['type' => config('article_type.projects')])->paginate(4);
+        $products = db('article')->field('id,title,order,pic,time,keywords,see,desc')->where(['type' => config('article_type.projects')])->paginate(9);
 
         $this->view->assign([
             'recent'   => $recent,
