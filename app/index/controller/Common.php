a43461c3fc54106f127412562b4aa97a49aeb902
FerreWagner <18408229270@163.com>
2019-02-25 23:17:49 +0800

    add admin artsee function

--- app/index/controller/Common.php
+++ app/index/controller/Common.php
@@ -16,6 +16,7 @@ class Common extends Controller
             echo '<h2 style="text-align: center;margin-top: 200px;">Alethea已关闭，谢谢</h2>';
             die;
         }
+        $this->artSee();
 
         $this->view->assign([
             'recent'   => $recent,
@@ -36,6 +37,114 @@ class Common extends Controller
             'post'  => $post,
         ]);
         return $this->view->fetch('post/post');
+    }
+
+    public function artSee()
+    {
+         $see_time  = db('artsee')->field('time')->where('ip', request()->ip())->order('time', 'desc')->find();
+         if ((time() - $see_time['time']) > 30){
+             $see = [
+                 'type'     => $this->getBrowser(),
+                 'rid'      => 1,
+                 'ip'       => request()->ip(),
+                 'country'  => '',
+                 'province' => '',
+                 'city'     => '',
+                 'time'     => time(),
+             ];
+             db('artsee')->insert($see);
+         }
+
+//         if (empty($see_time)){    //(time() - $see_time['time']) > 30 ||
+//
+//             //sina地理位置接口
+//             //&改为'(&)'或%26
+//             $area      = file_get_contents("http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip={$request->ip()}");
+//             $arr_data  = json_decode($area, true);
+//             $error     = json_last_error();
+//
+//             //json是否存在错误
+//             if (!empty($error)) {
+//                 $see = [
+//                     'type'     => $this->getBrowser(),
+//                     'rid'      => $rid,
+//                     'ip'       => $request->ip(),
+//                     'country'  => '',
+//                     'province' => '',
+//                     'city'     => '',
+//                     'time'     => time(),
+//                 ];
+//             }else {
+//                 $see = [
+//                     'type'     => $this->getBrowser(),
+//                     'rid'      => $rid,
+//                     'ip'       => $request->ip(),
+//                     'country'  => $arr_data['country'],
+//                     'province' => $arr_data['province'],
+//                     'city'     => $arr_data['city'],
+//                     'time'     => time(),
+//                 ];
+//             }
+//             db('artsee')->insert($see);
+//         }
+
+    }
+
+    /**
+     * user get browser
+     * @return string
+     */
+    public function getBrowser()
+    {
+
+        switch ($_SERVER['HTTP_USER_AGENT'])
+        {
+            case null:
+                return 'machine';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 9.0'):
+                return 'ie9';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 8.0'):
+                return 'ie8';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 7.0'):
+                return 'ie7';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'MSIE 6.0'):
+                return 'ie6';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'Firefox'):
+                return 'fox';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'Chrome'):
+                return 'chrome';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'Safari'):
+                return 'safari';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'Opera'):
+                return 'opera';
+                break;
+
+            case false !== strpos($_SERVER['HTTP_USER_AGENT'],'360SE'):
+                return '360se';
+                break;
+
+            default:
+                return 'notidentify';
+                break;
+
+        }
+
     }
     
     
